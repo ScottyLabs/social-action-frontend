5380d69fd594e5b385cc91eeeb362497be1c9c0e
Shannon Ding <shannon_ding@yahoo.com>
2020-11-07 15:21:15 -0500

    react version of page with spotlight and clock

---
+++ src/Overview.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Time from './Time';
+import Support from './Support';
+
+import './Overview.css';
+
+function Overview(props) {
+
+	return (
+		<div className="overview">
+			<Time />
+			<div className="overview-body">
+			<Support />
+			<input type="text" />
+			</div>
+			</div>
+		);
+}
+
+export default Overview;
---
+++ src/Tags.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Tags() {
+	return <div className="subtitle">Tags: food, corn</div>;
+}
+export default Tags;
