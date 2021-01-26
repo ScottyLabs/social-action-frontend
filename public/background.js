6e0fbd41e5ac2dfb172884d159f62131343e8dda
Rita Zhang <ritazhang@Ritas-MacBook-Pro.local>
2021-01-25 21:08:27 -0500

    added settings pop up

--- public/background.js
+++ public/background.js
@@ -1,3 +1,8 @@
 chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.create({ url: "chrome://newtab" });
 });
+
+chrome.storage.sync.set({ restaurant: false }, function () {
+  // Notify that we saved.
+  console.log("initialization");
+});
--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,38 +1,101 @@
+/* global chrome */
 import React, { useState } from "react";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 import "./Preferences.css";
 
 function Preferences(props) {
-  const [modalIsOpen,setIsOpen] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [recRestaurant, setRestaurant] = useState(false);
+
+  const [recBeauty, setBeauty] = useState(false);
+  const [recEducation, setEducation] = useState(false);
+
+  chrome.storage.sync.get({ restaurant: false }, function (result) {
+    setRestaurant(result.restaurant);
+    console.log(recRestaurant);
+  });
+
+  chrome.storage.sync.get({ beauty: false }, function (result) {
+    setBeauty(result.beauty);
+    // console.log(recRestaurant);
+  });
+
+  chrome.storage.sync.get({ education: false }, function (result) {
+    setEducation(result.education);
+    // console.log(recRestaurant);
+  });
+
+  function handleRestaurantChange() {
+    setRestaurant(!recRestaurant);
+    chrome.storage.sync.set({ restaurant: !recRestaurant }, function () {
+      // Notify that we saved.
+      console.log(recRestaurant);
+      console.log("Settings saved");
+    });
+  }
+
+  function handleBeautyChange() {
+    setBeauty(!recBeauty);
+    chrome.storage.sync.set({ beauty: !recBeauty }, function () {
+      // Notify that we saved.
+      console.log(recBeauty);
+      console.log("Settings saved");
+    });
+  }
+
+  function handleEducationChange() {
+    setEducation(!recEducation);
+    chrome.storage.sync.set({ education: !recEducation }, function () {
+      // Notify that we saved.
+      console.log(recEducation);
+      console.log("Settings saved");
+    });
+  }
 
   function openModal() {
     setIsOpen(true);
   }
 
-  function closeModal(){
+  function closeModal() {
     setIsOpen(false);
   }
-  
 
   return (
-    <div className="preferences">
-      <button id = "pref-button" onClick={openModal}>
-          Preferences   
+    <div class="preferences">
+      <button id="pref-button" onClick={openModal}>
+        Settings
       </button>
-      <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          className="Modal"
-        >
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="Modal">
+        <h1>Settings</h1>
 
-          <h1 >Preferences</h1>
-          
-          <div> Color </div>
-          <form>
-            <input type="text" id="color-input"/>
-          </form>
-          <button onClick={closeModal}>close</button>
-        </Modal>
+        <form class="settings-form">
+          <h2>Types of Recommendations </h2>
+          <label>
+            Restaurant:
+            <input
+              type="checkbox"
+              onChange={handleRestaurantChange}
+              checked={recRestaurant}
+            />
+          </label>
+          <label>
+            Beauty:
+            <input
+              type="checkbox"
+              onChange={handleBeautyChange}
+              checked={recBeauty}
+            />
+          </label>
+          <label>
+            Education:
+            <input
+              type="checkbox"
+              onChange={handleEducationChange}
+              checked={recEducation}
+            />
+          </label>
+        </form>
+      </Modal>
     </div>
   );
 }
